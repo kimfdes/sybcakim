b5a86b1df25027daa23da2314d036bdfce8fb0f8
kim fernandes <kimsfdf@gmail.com>
2024-01-20 10:05:52 +0530

    comit add

--- New folder/sybca205/src/sybca205/add.java
+++ New folder/sybca205/src/sybca205/add.java
@@ -5,6 +5,17 @@ public class add {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	int num1,num2;
+	num1=20;
+	num2=30;
+	int sum=num1+num2;
+	System.out.println(""+sum);
+		
+		
+		
+		
+		
+		
 	}
 
 }
